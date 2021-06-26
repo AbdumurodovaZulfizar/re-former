d9589183ab2fb382d9a420da169f19d9e3bf1cd2
AbdumurodovaZulfizar <abdumurodovazulfizar77@gamil,com>
2021-06-26 16:55:15 +0500

    Add validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+
+  validates :email, confirmation: true
+  validates :email_confirmation, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
